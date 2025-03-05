569f7edcd4b42bc48e62b1b28d07e31aa5fae678
Matías Morales <programacionfrida@gmail.com>
2025-03-05 20:15:22 -0300

    get category expense

---
+++ src/handlers/category/gets/expense/getExpenseCategories.js
@@ -0,0 +1,14 @@
+const getController = require('../../../../controllers/category/gets/expense/getCategories.js');
+
+const getExpenseCategoriesHandler = async (req, res) => {
+
+    try {
+        const categories = await getController();
+        res.status(200).send(categories);
+
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+};
+
+module.exports = getExpenseCategoriesHandler;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,15 @@ const router = Router();
 const clientRouter = require('./clientRouter.js');
 const incomeRouter = require('./incomeRouter.js');
 const expenseRouter = require('./expenseRouter.js');
+// const expenseCategoryRouter = require('./expenseCategoryRouter.js');
+
 
 
 router.use('/client', clientRouter);
 router.use('/income', incomeRouter);
 router.use('/expense', expenseRouter);
+// router.use('/expense/category', expenseCategoryRouter);
+
 router.use('/', async(req, res) => {
     res.send('Server working')
 });
