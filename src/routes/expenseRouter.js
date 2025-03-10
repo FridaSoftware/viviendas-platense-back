9e793236f97a9f97f48c0aeef82e13ad6533bf57
Matías Morales <programacionfrida@gmail.com>
2025-03-10 14:21:44 -0300

    expenseRouter modified ok

--- src/routes/expenseRouter.js
+++ src/routes/expenseRouter.js
@@ -2,18 +2,18 @@ const expenseRouter = require('express').Router();
 const { getExpenses, getExpenseById, postExpense, putExpense, putExpenseStatus } = require('../handlers/cash/index.js');
 const { getExpenseCategories, postExpenseCategory, putExpenseCategory, putExpenseCategoryStatus } = require('../handlers/category/index.js')
 
-expenseRouter.get('/', getExpenses);
-expenseRouter.get('/:id', getExpenseById);
-expenseRouter.post('/', postExpense);
-expenseRouter.put('/', putExpense);
-expenseRouter.put('/:id', putExpenseStatus);
-
-
 //--- CATEGORY
 expenseRouter.get('/category', getExpenseCategories);
 expenseRouter.post('/category', postExpenseCategory);
 expenseRouter.put('/category', putExpenseCategory);
 expenseRouter.put('/category/:id', putExpenseCategoryStatus);
+
+//--- EXPENSE
+expenseRouter.get('/', getExpenses);
+expenseRouter.get('/:id', getExpenseById);
+expenseRouter.post('/', postExpense);
+expenseRouter.put('/', putExpense);
+expenseRouter.put('/:id', putExpenseStatus);
 
 
 module.exports = expenseRouter;
