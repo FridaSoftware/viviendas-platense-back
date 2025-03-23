0a7a505701acb21322c1ed0e961950418430243e
Matías Morales <programacionfrida@gmail.com>
2025-03-23 18:53:15 -0300

    getIncomesByMonthAndYear ok

---
+++ src/controllers/cash/income/gets/getIncomesByMonthAndYear.js
@@ -0,0 +1,23 @@
+require('../../../../db.js');
+const Income = require('../../../../models/Income.js');
+
+const getIncomesByMonthAndYearCtrl = async (month, year) => {
+    const filter = { active: true };
+
+    if (year && month) {
+        const formattedMonth = month.toString().padStart(2, "0"); // Asegurar dos dígitos en el mes
+        const yearString = year.toString();
+
+        // Buscar fechas que coincidan con el formato "MM/YYYY"
+        filter.date = { $regex: new RegExp(`^\\d{2}/${formattedMonth}/${yearString}$`) };
+    } else if (year) {
+        const yearString = year.toString();
+        // Buscar cualquier fecha que termine en "/YYYY"
+        filter.date = { $regex: new RegExp(`/${yearString}$`) };
+    }
+
+    const incomesByMonthAndYear = await Income.find(filter);
+    return incomesByMonthAndYear;
+};
+
+module.exports = getIncomesByMonthAndYearCtrl;
