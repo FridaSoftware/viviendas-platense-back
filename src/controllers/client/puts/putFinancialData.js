5bc1f73061eec0816972c8e47c512319f5747e45
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-04-11 17:32:59 -0300

    payments and installments crossed data withincomes done/downpayment edit fixed

--- src/controllers/client/puts/putFinancialData.js
+++ src/controllers/client/puts/putFinancialData.js
@@ -1,10 +1,9 @@
 const Client = require('../../../models/Client.js');
 const getIncomeCategories = require('../../category/income/gets/getCategories.js');
 const postIncomeCategory = require('../../category/income/posts/postIncomeCategory.js');
+const getIncomeByDescription = require('../../cash/income/gets/getIncomeByDescription.js');
 const postIncome = require('../../cash/income/posts/postIncome.js');
-const getExpenseCategories = require('../../category/expense/gets/getCategories.js');
-const postExpenseCategory = require('../../category/expense/posts/postExpenseCategory.js');
-const postExpense = require('../../cash/expense/posts/postExpense.js');
+const putIncome = require('../../cash/income/puts/putIncome.js');
 
 const putFinancialDataCtrl = async (client, vendor, totalCost, downPayment) => {
     
@@ -17,18 +16,12 @@ const putFinancialDataCtrl = async (client, vendor, totalCost, downPayment) => {
     const updatedClient = await Client.findOneAndUpdate({ _id: client._id }, { $set: update }, { new: true });
 
     if(updatedClient && client.financialData.downPayment && downPayment ){
-        const categories = await getExpenseCategories();
 
-        let cancelationCategory = categories.find((category) => category.name === "Cancelación");
+        const income = await getIncomeByDescription(`Seña - Cliente N°${updatedClient.number}`);
 
-        if(!cancelationCategory){
-            cancelationCategory = await postExpenseCategory("Cancelación");
-        }
+        const updatedIncome = await putIncome(income._id, downPayment.paidDate, downPayment.finalAmount, downPayment.currency, downPayment.paymentMethod, income.category, `Seña - Cliente N°${updatedClient.number}`);
 
-        const newExpense = await postExpense(downPayment.paidDate, client.financialData.downPayment.finalAmount, client.financialData.downPayment.currency, client.financialData.downPayment.paymentMethod, cancelationCategory._id, `Cambio en la seña - ${updatedClient.personalData.name}`);
-    }
-    
-    if(updatedClient && downPayment){
+    } else if(updatedClient && downPayment){
         const categories = await getIncomeCategories();
 
         let downPaymentCategory = categories.find((category) => category.name === "Seña");
@@ -37,7 +30,7 @@ const putFinancialDataCtrl = async (client, vendor, totalCost, downPayment) => {
             downPaymentCategory = await postIncomeCategory("Seña");
         }
 
-        const newIncome = await postIncome(downPayment.paidDate, downPayment.finalAmount, downPayment.currency, downPayment.paymentMethod, downPaymentCategory._id, `Seña - ${updatedClient.personalData.name}`);
+        const newIncome = await postIncome(downPayment.paidDate, downPayment.finalAmount, downPayment.currency, downPayment.paymentMethod, downPaymentCategory._id, `Seña - Cliente N°${updatedClient.number}`);
     };
 
     return updatedClient;
