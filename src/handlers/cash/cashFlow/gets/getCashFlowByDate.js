1e199d127e317e6e0e68a1038a2089365345c2dc
Matías Morales <programacionfrida@gmail.com>
2025-05-08 17:21:23 -0300

    filter cashflow by date category ok

--- src/handlers/cash/cashFlow/gets/getCashFlowByDate.js
+++ src/handlers/cash/cashFlow/gets/getCashFlowByDate.js
@@ -1,13 +1,13 @@
 const getController = require('../../../../controllers/cash/cashFlow/gets/getCashFlowByDate.js');
 
 const getCashFlowByDateHandler = async (req, res) => {
-    const { start, end } = req.query;  
+    const { start, end, categoryId } = req.query;  
 
     try {
-        const cashFlowByDate = await getController(start, end);
+        const cashFlowByDate = await getController(start, end, categoryId);
 
         if (!cashFlowByDate) {
-            return res.status(404).send(`No service cash flow found between dates: ${start} - ${end}`);
+            return res.status(404).send(`No service cash flow found between dates: ${start} - ${end} - ${categoryId}`);
         }
 
         res.status(200).send(cashFlowByDate);
