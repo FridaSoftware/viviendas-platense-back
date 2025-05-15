5d52e4e0131de8e0fabd382f0c6bbff05ed25d8c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-05-15 15:05:55 -0300

    filtered get cash flow done

--- src/controllers/cash/cashFlow/gets/getCashFlow.js
+++ src/controllers/cash/cashFlow/gets/getCashFlow.js
@@ -3,8 +3,8 @@ const Income = require('../../../../models/Income.js');
 const Expense = require('../../../../models/Expense.js');
 
 const getCashFlowCtrl = async () => {
-    const incomes = await Income.find().populate('category', 'name');
-    const expenses = await Expense.find().populate('category', 'name');
+    const incomes = await Income.find({active: true}).populate('category', 'name');
+    const expenses = await Expense.find({active: true}).populate('category', 'name');
 
     // Agregar una propiedad 'type' a cada ingreso y egreso para distinguirlos luego
     const incomesWithType = incomes.map(income => ({
